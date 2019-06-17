302d4dbc94dcfc0b025496675411d142a7ea733f
David Crook <idcrook@users.noreply.github.com>
2019-06-16 18:33:28 -0600

    prepping for javascript overhaul

--- elisp/lang-web.el
+++ elisp/lang-web.el
@@ -27,32 +27,35 @@
         web-mode-css-indent-offset 2
         web-mode-code-indent-offset 2)
 
-  (add-hook 'web-mode-hook 'jsx-flycheck)
+  ;; (add-hook 'web-mode-hook 'jsx-flycheck)
 
   ;; highlight enclosing tags of the element under cursor
   (setq web-mode-enable-current-element-highlight t)
 
-  ;; https://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking
-  (defadvice web-mode-highlight-part (around tweak-jsx activate)
-    (if (equal web-mode-content-type "jsx")
-        (let ((web-mode-enable-part-face nil))
-          ad-do-it)
-      ad-do-it))
+  ;; ;; https://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking
+  ;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+  ;;   (if (equal web-mode-content-type "jsx")
+  ;;       (let ((web-mode-enable-part-face nil))
+  ;;         ad-do-it)
+  ;;     ad-do-it))
 
-  ;; npm install -g jsxhint
-  (flycheck-define-checker jsxhint-checker
-    "A JSX syntax and style checker based on JSXHint."
+  ;; jsxhint is deprecated and broken 2019-Jun-15
+  ;; ReferenceError: primordials is not defined
+  ;; ;; npm install --global gulp-cli
+  ;; ;; npm install -g jsxhint
+  ;; (flycheck-define-checker jsxhint-checker
+  ;;   "A JSX syntax and style checker based on JSXHint."
 
-    :command ("jsxhint" "--harmony" source)
-    :error-patterns
-    ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-    :modes (web-mode))
+  ;;   :command ("jsxhint" "--harmony" source)
+  ;;   :error-patterns
+  ;;   ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  ;;   :modes (web-mode))
 
-  (defun jsx-flycheck ()
-    (when (equal web-mode-content-type "jsx")
-      ;; enable flycheck
-      (flycheck-select-checker 'jsxhint-checker)
-      (flycheck-mode)))
+  ;; (defun jsx-flycheck ()
+  ;;   (when (equal web-mode-content-type "jsx")
+  ;;     ;; enable flycheck
+  ;;     (flycheck-select-checker 'jsxhint-checker)
+  ;;     (flycheck-mode)))
 
   ;; ;; editing enhancements for web-mode
   ;; ;; https://github.com/jtkDvlp/web-mode-edit-element
@@ -83,33 +86,33 @@
 ;; ;; npm -g install js-beautify
 ;; (use-package web-beautify)
 
-;;; for vue.js / .vue files - https://github.com/AdamNiederer/vue-mode
-(use-package vue-mode)
+;; ;;; for vue.js / .vue files - https://github.com/AdamNiederer/vue-mode
+;; (use-package vue-mode)
 
-(use-package rjsx-mode
-  :mode "\\.jsx$"
-  :mode "components/.+\\.js$"
-  :init
-  ;; auto-detect JSX file
-  ;; https://github.com/shahinism/emacs.d/blob/master/configs.org
-  ;; source: https://github.com/hlissner/.emacs.d/blob/master/modules/lang/javascript/config.el
-  (push (cons (lambda ()
-                (and buffer-file-name
-                     (equal (file-name-extension buffer-file-name) "js")
-                     (re-search-forward "\\(^\\s-*import React\\|\\( from \\|require(\\)[\"']react\\)"
-                                        magic-mode-regexp-match-limit t)
-                     (progn
-                       (goto-char (match-beginning 1))
-                       ;; sp-point-in-string-or-comment requires smartparens package
-                       (not (sp-point-in-string-or-comment)))))
-              'rjsx-mode)
-        magic-mode-alist)
-  :config
-  ;; match gatsby.js layout
-  (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
-  (add-to-list 'auto-mode-alist '("layouts\\/.*\\.js\\'" . rjsx-mode))
-  (add-to-list 'auto-mode-alist '("templates\\/.*\\.js\\'" . rjsx-mode))
-  (add-to-list 'auto-mode-alist '("pages\\/.*\\.js\\'" . rjsx-mode)))
+;; (use-package rjsx-mode
+;;   :mode "\\.jsx$"
+;;   :mode "components/.+\\.js$"
+;;   :init
+;;   ;; auto-detect JSX file
+;;   ;; https://github.com/shahinism/emacs.d/blob/master/configs.org
+;;   ;; source: https://github.com/hlissner/.emacs.d/blob/master/modules/lang/javascript/config.el
+;;   (push (cons (lambda ()
+;;                 (and buffer-file-name
+;;                      (equal (file-name-extension buffer-file-name) "js")
+;;                      (re-search-forward "\\(^\\s-*import React\\|\\( from \\|require(\\)[\"']react\\)"
+;;                                         magic-mode-regexp-match-limit t)
+;;                      (progn
+;;                        (goto-char (match-beginning 1))
+;;                        ;; sp-point-in-string-or-comment requires smartparens package
+;;                        (not (sp-point-in-string-or-comment)))))
+;;               'rjsx-mode)
+;;         magic-mode-alist)
+;;   :config
+;;   ;; match gatsby.js layout
+;;   (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+;;   (add-to-list 'auto-mode-alist '("layouts\\/.*\\.js\\'" . rjsx-mode))
+;;   (add-to-list 'auto-mode-alist '("templates\\/.*\\.js\\'" . rjsx-mode))
+;;   (add-to-list 'auto-mode-alist '("pages\\/.*\\.js\\'" . rjsx-mode)))
 
 ;; configure CSS mode company backends
 (use-package css-mode
@@ -120,11 +123,11 @@
   (add-hook 'css-mode-hook 'my-css-mode-hook)
   (add-hook 'css-mode-hook 'company-mode))
 
-;; impatient mode - Live refresh of web pages
-;; https://github.com/skeeto/impatient-mode
-(use-package impatient-mode
-  :diminish (impatient-mode . " i")
-  :commands (impatient-mode))
+;; ;; impatient mode - Live refresh of web pages
+;; ;; https://github.com/skeeto/impatient-mode
+;; (use-package impatient-mode
+;;   :diminish (impatient-mode . " i")
+;;   :commands (impatient-mode))
 
 (provide 'lang-web)
 
