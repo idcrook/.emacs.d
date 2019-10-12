5ddf891664917a68375b4c424d0afa92fc75471d
David Crook <idcrook@users.noreply.github.com>
2019-10-12 11:34:59 -0600

    merge custom.el from macos and linux terminal usages

--- custom.el
+++ custom.el
@@ -5,7 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("1a232652b04b68380b1cff7ceeb62787b4eb43df826a97c67831c50b0c0d1451" default))))
+    ("1a232652b04b68380b1cff7ceeb62787b4eb43df826a97c67831c50b0c0d1451" default)))
+ '(safe-local-variable-values
+   '((eval sh-set-shell "zsh")
+     (ruby-compilation-executable . "ruby")
+     (ruby-compilation-executable . "ruby1.8")
+     (ruby-compilation-executable . "ruby1.9")
+     (ruby-compilation-executable . "rbx")
+     (ruby-compilation-executable . "jruby"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
