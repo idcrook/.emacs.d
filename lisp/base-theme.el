70c6ab0f3d3916611caa542f57275411448f4dfb
David Crook <idcrook@users.noreply.github.com>
2025-09-16 14:45:11 -0600

    comment out magit git executable-find

--- lisp/base-theme.el
+++ lisp/base-theme.el
@@ -8,83 +8,44 @@
 ;; first-time startup on Emacs > 26.3.
 (setq custom-safe-themes t)
 
-;; Trial themes
-;;; https://github.com/purcell/color-theme-sanityinc-tomorrow
-;;(use-package color-theme-sanityinc-tomorrow)
-;; sanityinc-tomorrow-blue is like Wordperfect
-;; sanityinc-tomorrow-bright is actually a dark theme
-;;; https://github.com/n3mo/cyberpunk-theme.el
-;; (use-package cyberpunk-theme)
-;; ;;; https://github.com/NicolasPetton/zerodark-theme
-;; (use-package zerodark-theme)
-;;    ;; :config
-;;    ;; ;; Optionally setup the modeline
-;;    ;; (zerodark-setup-modeline-format)
-;;   )
-;; ;;; https://github.com/mgrbyte/emacs-abyss-theme
-;; (use-package abyss-theme)
-;; ;;; https://github.com/osener/emacs-afternoon-theme
-;; (use-package afternoon-theme)
-;;; https://github.com/dracula/dracula-theme
-;;; https://draculatheme.com/emacs/
-;; (use-package dracula-theme)
-;; ;;; https://github.com/rexim/gruber-darker-theme
-;; (use-package gruber-darker-theme)
-;;; https://github.com/alezost/alect-themes
-;; (use-package alect-themes)
-;; (setq alect-display-class '((class color) (min-colors 256)))
-
-
-;; [Ef (εὖ) themes for GNU Emacs | Protesilaos Stavrou](https://protesilaos.com/emacs/ef-themes#h:dd9e06f2-eef0-4afe-8a12-b7af5d597108)
-(use-package ef-themes
-  :config
-  ;; command : ef-themes-toggle
-  (setq ef-themes-to-toggle '(ef-night ef-maris-dark))
-)
-
-;; ;;; [Modus Themes (Modus Operandi and Modus Vivendi) | Protesilaos Stavrou](https://protesilaos.com/modus-themes/)
+;; ;;; https://protesilaos.com/modus-themes/
 ;; (use-package modus-themes
-;;   :config
-;;   ;; Add all your customizations prior to loading the themes
-;;   (setq         ;; modus-themes-bold-constructs nil
-;;                 ;; modus-themes-slanted-constructs t
-;;                 modus-themes-bold-constructs t
-;;                 modus-themes-mode-line '(3d accented))
-;;   ;; ;; Old way.
-;;   ;; (if (fboundp 'modus-themes-load-themes)
-;;   ;;     (modus-themes-load-themes))
-;;   ;;  :bind ("<f5>" . modus-themes-toggle)
-;;   ;; See 'dpc-load-theme for function to load theme
-;; )
+;;   ;;:straight (:type git :host github :repo "emacs-straight/modus-themes")
+;;   :straight (:type git :host github :repo "protesilaos/modus-themes")
+;;   )
 
-;; ;;; https://github.com/d12frosted/homebrew-emacs-plus#system-appearance-change
-;; ;; FIXME: handle light/dark mode
-;; (defun my/apply-theme (appearance)
-;;   "Load theme, taking current system APPEARANCE into consideration."
-;;   (mapc #'disable-theme custom-enabled-themes)
-;;   (pcase appearance
-;;     ('light (load-theme 'tango t))
-;;     ('dark (load-theme 'tango-dark t))))
-;; (add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
+;;; https://protesilaos.com/emacs/ef-themes
+(use-package ef-themes
+  ;;  :straight (:type git :host github :repo "emacs-straight/ef-themes")
+  :straight (:type git :host github :repo "protesilaos/ef-themes")
+  :init
+  (ef-themes-take-over-modus-themes-mode 1)
+  :bind
+  (("<f5>" . modus-themes-rotate)
+   ("C-<f5>" . modus-themes-select)
+   ("M-<f5>" . modus-themes-load-random-dark))
+  :config
+  ;; Add all your customizations prior to loading the themes
+  (setq
+   ;;modus-themes-bold-constructs nil
+   ;;modus-themes-slanted-constructs t
+   modus-themes-italic-constructs t
+   ;;modus-themes-bold-constructs t
+   ;;modus-themes-mode-line '(3d accented)
+   )
+   (setq modus-themes-to-toggle '(ef-night ef-maris-dark))
+  )
+
 
 (defun dpc-load-theme ()
     "Set current theme, with preferences on GUI or terminal."
   (interactive)
   ;; use a different theme, depending on graphical window or (tty/console)
   (if (display-graphic-p)
-      ;; (load-theme 'abyss 'no-confirm)
-      ;; (load-theme 'zerodark 'no-confirm)
-      ;; (load-theme 'afternoon 'no-confirm)
-      ;; (load-theme 'dracula 'no-confirm)
-      ;; (load-theme 'gruber-darker 'no-confirm)
       ;; Preferred ;; (load-theme 'sanityinc-tomorrow-bright 'no-confirm)
-      ;; ;; Old way
-      ;; (if (fboundp 'modus-themes-load-themes)
-      ;;     (modus-themes-load-vivendi)
-      ;;   (load-theme 'modus-vivendi :no-confirm))
-      ;;(load-theme 'modus-vivendi :no-confirm)
       ;; load the theme which also calls `ef-themes-post-load-hook':
-      (ef-themes-select 'ef-night)
+      ;;(ef-themes-select 'ef-night)
+      (modus-themes-select 'ef-night)
 
     ;; tty
     (load-theme 'tango-dark)  ;; tango-dark included in emacs
